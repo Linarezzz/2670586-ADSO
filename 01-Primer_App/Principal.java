31a2b76e5b6cd081b77d29099a1198ae1222722b
INSTRUCTOR <INSTRUCTOR@PERAPRCSAFSD525.FormacionSena.RED>
2023-05-05 17:41:53 -0500

    Primer prueba de java

---
+++ 01-Primer_App/Principal.java
@@ -0,0 +1,27 @@
+public class Principal{    // Todos los archivos java deben empezar con esta estructura, principal es por que el nombre del archivo es ese, sino cambia
+    public static void main(String [] args){
+
+        System.out.println( "++-------------------------------++" );   //System.out.println es para escribir algún texto //
+        System.out.println( "++---  SISTEMA DE FACURACION  ---++" );
+        System.out.println( "++-------------------------------++" );
+        System.out.println( "||                               ||" );
+        System.out.println( "||                  Clientes:89  ||" );
+        System.out.println( "||               Vendedores:13   ||" );
+        System.out.println( "||                Productos:130  ||" );
+        System.out.println( "||                 Facturas:30   ||" );
+        System.out.println( "||                               ||" );
+        System.out.println( "||  1. Creacion de cliente       ||" );
+        System.out.println( "||  2. Creacion de vendedor      ||" );
+        System.out.println( "||  3. Creacion de factura       ||" );
+        System.out.println( "||  4. Ver lista de clientes     ||" );
+        System.out.println( "||  5. Ver lista de vendedores   ||" );
+        System.out.println( "||  6. Ver lista de facturas     ||" );
+        System.out.println( "||  7. Ver detalle de factura    ||" );
+        System.out.println( "||  8. Salir                     ||" );
+        System.out.println( "||                               ||" );
+        System.out.println( "++-------------------------------++" );
+        System.out.println( "++-------------------------------++" );
+        
+
+    }
+}
