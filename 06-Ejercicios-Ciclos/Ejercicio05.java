539a0cf1b1231e2ef0e0867e3fd2ce5f04f65265
Juanpa Linares <juampix159@gmail.com>
2023-06-15 13:36:18 -0500

    Ejercicio 5

--- 06-Ejercicios-Ciclos/Ejercicio05.java
+++ 06-Ejercicios-Ciclos/Ejercicio05.java
@@ -5,7 +5,22 @@ public class Ejercicio05 {
 
         Scanner teclado = new Scanner(System.in);
 
-        System.out.println("Ingrese un numero: ");
+        System.out.print("Ingrese un numero: ");
         int numero = teclado.nextInt();
+
+        int numero_anterior = 0;
+        int numero_siguiente = 1;
+
+        System.out.println("Sucesión de Fibonacci hasta el número " + numero + ":");
+        System.out.print(numero_anterior + " ");
+
+        for (int i = 0; i < numero; i++) {
+            System.out.print(numero_anterior + " ");
+            int siguiente = numero_anterior + numero_siguiente;
+            numero_anterior = numero_siguiente;
+            numero_siguiente = siguiente;
+        }
+
+        System.out.println();
     }
 }
