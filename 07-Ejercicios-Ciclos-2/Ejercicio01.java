e6303747faa708a8c99c30e784a8727649785c30
MAC2490 <mauricioaguirre2490@gmail.com>
2023-06-16 17:25:18 -0500

    Cambios ejercicio 1

--- 07-Ejercicios-Ciclos-2/Ejercicio01.java
+++ 07-Ejercicios-Ciclos-2/Ejercicio01.java
@@ -5,7 +5,22 @@ public class Ejercicio01{
 
         Scanner teclado = new Scanner(System.in);
 
+        int contador = 1;
+        double numero_aleatorio = 0;
+        double producto = 0;
+
         System.out.print("Ingrese un numero entre 1 y 6: ");
         int numero = teclado.nextInt();
+
+        if( numero <= 0 || numero > 6){
+            System.out.println("El numero no es válido");
+        }else{
+            while(contador <= numero){
+                numero_aleatorio = Math.random() * 9;
+                contador = contador + 1;
+                System.out.print(" "+numero_aleatorio);
+            }
+
+        }
     }
 }
