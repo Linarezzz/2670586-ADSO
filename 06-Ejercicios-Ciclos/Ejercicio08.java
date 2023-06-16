7f4580a4a0df719a8e701b955a90542ecc38f1a0
MAC2490 <mauricioaguirre2490@gmail.com>
2023-06-16 17:01:07 -0500

    Ejercicios 8 y 10 pendientes

--- 06-Ejercicios-Ciclos/Ejercicio08.java
+++ 06-Ejercicios-Ciclos/Ejercicio08.java
@@ -4,6 +4,8 @@ public class Ejercicio08{
     public static void main(String[] args){
 
         Scanner teclado = new Scanner(System.in);
-        
+
+
+        // PENDIENTE POR HACER 
     }
 }
---
+++ 06-Ejercicios-Ciclos/Ejercicio10.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ejercicio10{
+    public static void main(String[] args){
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("--> Ingresar numero: ")
+        int numero = teclado.nextInt();
+
+        //PENDIENTE POR TERMINAR
+    }
