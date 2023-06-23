a520cf4f5a99a2a881dbf563cfe719e03ca00b91
Juanpa Linares <juampix159@gmail.com>
2023-06-22 23:18:55 -0500

    Ejercicio 8

--- 06-Ejercicios-Ciclos/Ejercicio08.java
+++ 06-Ejercicios-Ciclos/Ejercicio08.java
@@ -5,7 +5,29 @@ public class Ejercicio08{
 
         Scanner teclado = new Scanner(System.in);
 
+        System.out.print("Ingresar un número: ");
+        int numero = teclado.nextInt();
 
-        // PENDIENTE POR HACER 
+        if (numero < 0 || numero > 999999) {
+            System.out.println("El número ingresado está fuera del rango");
+        }
+
+        System.out.println("------------------------------------");
+
+        int divisor = 100000;
+        int contador = 1;
+
+        while (divisor >= 1) {
+            int digito = numero / divisor;
+            System.out.println("-> El dígito " + contador + " es: " + digito);
+            numero = numero % divisor;
+            divisor = divisor / 10;
+            contador++;
+        }
+
+        System.out.println("------------------------------------------------------");
+
+
+        
     }
 }
