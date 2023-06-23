edf3dfc37fa59cd6d76844c6fa0c6cef4617bccd
MAC2490 <mauricioaguirre2490@gmail.com>
2023-06-23 17:46:36 -0500

    Avanzando ejercicio 2

--- 07-Ejercicios-Ciclos-2/Ejercicio02.java
+++ 07-Ejercicios-Ciclos-2/Ejercicio02.java
@@ -2,8 +2,18 @@ import java.util.Random;
 
 public class Ejercicio02{
     public static void main(String[] args) {
+        int año = (int) (Math.random() * 8000)+1000;
 
-        //PENDIENTE
-     
+        int mes = (int) (Math.random()* 12)+1;
+
+
+
+
+
+
+
+
+        System.out.print("Fecha: "+mes+","+año);
+
     }
 }
