e2a1cce5edde66af96d67e19fc5870a57661fae1
Juanpa Linares <juampix159@gmail.com>
2023-07-30 12:33:39 -0500

    Ejercicio 5

--- 11-Ejercicios-Arreglos-2/Ejercicio05.java
+++ 11-Ejercicios-Arreglos-2/Ejercicio05.java
@@ -5,22 +5,35 @@ public class Ejercicio05{
 
         //Solicitar al usuario ingresar un número N, crear un arreglo de tamaño N, llenarlo con datos aleatorios flotantes y
         //calcular el promedio de todos los elementos.
+        float suma = 0;
+        float promedio = 0;
 
 
-    Scanner teclado = new Scanner(System.in);
+        Scanner teclado = new Scanner(System.in);
 
-    System.out.print("Ingrese un numero: ");
-    int numero = teclado.nextInt();
+        System.out.print("Ingrese un numero: ");
+        int numero = teclado.nextInt();
 
-    // Arreglo, se cambia a tipo de dato
-    float arreglo [] = new float [numero];
+        // Arreglo, se cambia a tipo de dato
+        float arreglo [] = new float [numero];
 
-    // Ciclo para llenar el arreglo
+        // Ciclo para llenar el arreglo
 
-    for(int i = 0; i <arreglo.length;i++){
-        arreglo[i] = (float) (Math.random() * 100+1);
-        System.out.println("Los numeros flotantes generados son: "+arreglo[i]);
-    }
+        for(int i = 0; i <arreglo.length;i++){
+            arreglo[i] = (float) (Math.random() * 100+1);
+            System.out.println("Los numeros flotantes generados son: "+arreglo[i]);
+        }
+
+        // Ciclo para imprimir el arreglo
+
+        for(int i = 0; i < numero;i++){
+            suma = suma + arreglo[i];
+            promedio = numero;
+            promedio = suma / promedio;
+
+        }
+
+        System.out.print("El promedio de los numeros generados es: "+promedio);
     }
 
 }
