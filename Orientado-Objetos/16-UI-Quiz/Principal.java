03358c7b4ec657780814d3907d76d6226f958173
Jordy-int <jordy.sneide13@gmail.com>
2023-11-17 13:13:34 -0500

    avance quiz

---
+++ Orientado-Objetos/16-UI-Quiz/Principal.java
@@ -0,0 +1,6 @@
+public class Principal{
+    public static void main (String[] args){
+
+        Promedio ventana = new Promedio();
+    }
+}
---
+++ Orientado-Objetos/16-UI-Quiz/Promedio.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.text.NumberFormat;
+import java.util.Locale;
+import javax.swing.border.*;
+
+public class Promedio extends JFrame {
+    
+    //Atributos
+    private 
+}
