ae8f9e3983a03cfd6c25b53bc7f8d606304358e6
Juanpa Linares <juampix159@gmail.com>
2024-05-08 21:52:29 -0500

    Api pokemon completo (casi)

--- Api-pokemon-js/js/myscript.js
+++ Api-pokemon-js/js/myscript.js
@@ -1,38 +1,72 @@
 let contenPrincipal;
+let nombrePokemon;
+let imgPokemon;
+let anteriorEndPoint;
+let siguienteEndPoint;
+let paginaActual = 1;
 
-window.onload = function(){  // Sobreescribe el contenido de la pagina, solo si la pagina html esta cargada en la pagina del navegador
+window.onload = function(){  
     contenPrincipal = document.getElementById("contenPrincipal");
+    nombrePokemon = document.getElementById("nombrePokemon");
+    imgPokemon = document.getElementById("imgPokemon");
 }
 
 function consumoAPI(){
     let endpoint = "https://pokeapi.co/api/v2/pokemon";
 
     // Consumo - AJAX 
-    fetch(endpoint).then( res => res.json() ).then( data => { 
-
+    fetch(endpoint).then(res => res.json()).then(data => { 
         contenPrincipal.innerHTML = "";
 
-        // Codigo para ejecutar en caso de tener el JSON como respuesta de la API
-        for (let i=0; i < data.results.length; i++) {
-            
+        anteriorEndPoint = data.previous;
+        siguienteEndPoint = data.next;
+
+        for (let i = 0; i < data.results.length; i++) {
             let html_temp = `<div>
                                 <button onclick="cargarDetalle('${data.results[i].url}')" class="btn btn-outline-dark m-1 mt-0 col-12"> ${ data.results[i].name } </button>
                             </div>`;
-
             contenPrincipal.innerHTML += html_temp;                
         }
-
     });
-
-
 }
 
 function cargarDetalle(endpoint){
 
-    fetch(endpoint).then( respuesta => respuesta.json() ).then( data => { 
+    fetch(endpoint).then(respuesta => respuesta.json()).then(data => { 
 
         nombrePokemon.innerText = data.name;
         imgPokemon.src = data.sprites.other.dream_world.front_default;
     });
     
 }
+
+function cambiar_pagina(numero){
+    if (numero == -1 && anteriorEndPoint) {
+        paginaActual--;
+        consumirPagina(anteriorEndPoint);
+    } else if (numero == 1 && siguienteEndPoint){
+        paginaActual++;
+        consumirPagina(siguienteEndPoint);
+    }
+}
+
+function consumirPagina(endpoint){
+    fetch(endpoint).then(res => res.json()).then(data => { 
+        contenPrincipal.innerHTML = "";
+
+        anteriorEndPoint = data.previous;
+        siguienteEndPoint = data.next;
+
+        for (let i = 0; i < data.results.length; i++) {
+            let html_temp = `<div>
+                                <button onclick="cargarDetalle('${data.results[i].url}')" class="btn btn-outline-dark m-1 mt-0 col-12"> ${ data.results[i].name } </button>
+                            </div>`;
+            contenPrincipal.innerHTML += html_temp;                
+        }
+    });
+}
+
+
+
+
+
